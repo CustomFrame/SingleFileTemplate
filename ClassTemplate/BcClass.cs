56261b8efc7bfcf82b48375ef071d130063323de
BlueLover <214909012@qq.com>
2017-11-21 11:54:37 +0800

    文件头部附加版权信息

---
+++ ClassTemplate/BcClass.cs
@@ -0,0 +1,26 @@
+﻿/************************************************************************************
+ * Copyright (c) $year$ BlueLover All Rights Reserved.
+ * 公司名称：BlueLover
+ * 版本号：  V1.0.0.0
+ * 创建人：  梁智慧
+ * 电子邮箱：214909012@qq.com
+ * 创建时间：$time$
+ * 描述：
+ *
+ *=====================================================================
+/************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTemplate
+{
+    /// <summary>
+    /// 说明
+    /// </summary>
+    public class BcClass
+    {
+    }
+}
---
+++ ClassTemplate/BcClass1.cs
@@ -0,0 +1,26 @@
+﻿/************************************************************************************
+ * Copyright (c) 2017 BlueLover All Rights Reserved.
+ * 公司名称：BlueLover
+ * 版本号：  V1.0.0.0
+ * 创建人：  梁智慧
+ * 电子邮箱：214909012@qq.com
+ * 创建时间：2017/11/21 11:41:58
+ * 描述：
+ *
+ *=====================================================================
+/************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTemplate
+{
+    /// <summary>
+    /// 说明
+    /// </summary>
+    public class BcClass1
+    {
+    }
+}
